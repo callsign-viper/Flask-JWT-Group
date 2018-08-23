e6b414600388c6a71cd75df3f5ffd7cf9b094485
JeongKyungSeo <jerion7474@gmail.com>
2018-08-23 19:10:33 +0900

    [DONE] - DEFAULT_SETTINGS & create_access_token

--- flask_jwt_group/util.py
+++ flask_jwt_group/util.py
@@ -1,2 +1,23 @@
+import datetime
+import uuid
+
+import jwt
+from flask import current_app
+
+
 def create_access_token(identity, group=None):
-    pass
+    now = datetime.datetime.utcnow()
+    jti = str(uuid.uuid4())
+    secret = current_app.config['JWT_SECRET_KEY']
+    algorithm = current_app.config['JWT_ALGORITHM']
+    token = {
+        'iat': now,
+        'nbf': now,
+        'exp': current_app.config['JWT_ACCESS_TOKEN_EXPIRES'],
+        'jti': jti,
+        'identity': identity,
+        'type': 'access',
+        'group': group
+    }
+
+    return jwt.encode(token, secret, algorithm=algorithm).decode('utf-8')
