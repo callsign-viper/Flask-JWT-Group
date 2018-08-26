c9e840fb0b214e83f69d6ef68bd4b76e80b87a93
JoMingyu <city7310@naver.com>
2018-08-26 22:16:52 +0900

    [ADD] create_refresh_token, private function(_create_token)을 통한 추상화 추가

--- flask_jwt_group/util.py
+++ flask_jwt_group/util.py
@@ -1,23 +1,33 @@
-import datetime
-import uuid
+from datetime import datetime
+from uuid import uuid4
 
 import jwt
 from flask import current_app
 
 
-def create_access_token(identity, group=None):
-    now = datetime.datetime.utcnow()
-    jti = str(uuid.uuid4())
+def _create_token(identity, group=None, type='access'):
+    now = datetime.utcnow()
+    jti = str(uuid4())
     configs = current_app.config
+
     token = {
         'iat': now,
         'nbf': now,
         'exp': now + configs['JWT_ACCESS_TOKEN_EXPIRES'],
         'jti': jti,
         configs['JWT_IDENTITY_KEY']: identity,
-        'type': 'access',
-        configs['JWT_GROUP_KEY']: group
+        # default: 'identity'
+        configs['JWT_GROUP_KEY']: group,
+        # default: 'group'
+        'type': type,
     }
 
     return jwt.encode(token, key=configs['JWT_SECRET_KEY'], algorithm=configs['JWT_ALGORITHM']).decode('utf-8')
 
+
+def create_access_token(identity, group=None):
+    return _create_token(identity, group)
+
+
+def create_refresh_token(identity, group=None):
+    return _create_token(identity, group, 'refresh')
